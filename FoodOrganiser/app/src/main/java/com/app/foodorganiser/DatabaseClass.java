37a958842f2feb532a3b28221fb1b644adc036d4
Mikołaj <43763940+Mikoai@users.noreply.github.com>
2021-04-22 21:06:51 +0200

    cleaning up

--- FoodOrganiser/app/src/main/java/com/app/foodorganiser/DatabaseClass.java
+++
@@ -1,9 +0,0 @@
-package com.app.foodorganiser;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {ProductTable.class},version = 2,exportSchema = false)
-public abstract class DatabaseClass extends RoomDatabase {
-    public abstract ProductDAO productDAO();
-}
